05881859a683ad64c69e55923b2d77a16c946910
Sergey Seredkin <spseredkin@mail.ru>
2022-04-08 16:17:41 +0300

    Updates

---
+++ precache-manifest.dce36ed536321febbc2f6fae8156bee2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ff7cd933ba3da559c92",
+    "url": "/static/css/main.a859d7f4.chunk.css"
+  },
+  {
+    "revision": "8ff7cd933ba3da559c92",
+    "url": "/static/js/main.8ff7cd93.chunk.js"
+  },
+  {
+    "revision": "8778355862ca942e10cd",
+    "url": "/static/css/1.d5698bc5.chunk.css"
+  },
+  {
+    "revision": "8778355862ca942e10cd",
+    "url": "/static/js/1.87783558.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1e0ac0d74fa4e07e4371150f8119b4f0",
+    "url": "/static/media/phone.1e0ac0d7.jpg"
+  },
+  {
+    "revision": "b4053e9f2fa1b9922116be98774a6b23",
+    "url": "/index.html"
+  }
+];
